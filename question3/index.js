f0b8f6675f3248bad9b00658955f7d5c50f0e387
slickcodes <hartpaulisimo@gmail.com>
2023-02-21 14:04:44 +0100

    complete question 3

---
+++ question3/index.js
@@ -0,0 +1,27 @@
+
+
+
+function inventoryList() {
+    let inventory = [];
+
+    const isInventoryExist = name => {
+        // check if name exist in array
+        return Boolean(inventory.filter(inventoryName => inventoryName.toLowerCase().trim() === name.toLowerCase().trim()).length)
+    }
+
+    return {
+        add(inventoryName) {
+            if (isInventoryExist(inventoryName) || inventory.length >= 10)
+                return
+
+            inventory.push(inventoryName)
+        },
+        remove(inventoryName) {
+            // filer inventory and remove the parsed inventory
+            inventory = inventory.filter(invent => invent.toLowerCase().trim() !== inventoryName.toLowerCase().trim())
+        },
+        getList() { return inventory }
+    }
+}
+
+const inventory = inventoryList()
